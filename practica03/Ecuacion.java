ef333b634ddba9e9294df4f0910546e2789607e0
Claudia Gutierrez <clau_zel@invci-23-90.fciencias.unam.mx>
2019-08-20 11:59:06 -0500

    practica03

---
+++ practica03/Ecuacion.java
@@ -0,0 +1,18 @@
+public class Ecuacion {
+
+	public static void main(String[] args) {
+		double a = 3, b = 17, c = 10, d = -5.7, e = 3.5;
+		int i = 1, n = (100000000);
+		double suma = 0;
+		while (i <= n) {
+			/* Escribe en la línea 9 */
+			suma += (-550 * (a +(i*b-i*a)/n)*(a +(i*(b-a)/n)));
+			suma += (2992 * (a +(i*(b-a)/n))- (49*c*(d+e))/(34*(3*d +c))) ;
+
+			/* ************************ */
+			i++;
+		}
+		System.out.println("Resultado = " + (((b - a) / n) * suma));
+	}
+
+}
